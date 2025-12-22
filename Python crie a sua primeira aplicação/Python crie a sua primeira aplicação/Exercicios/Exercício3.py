125036730dac51818848a6667a3a3f342f6df6e8
MateusLeandroSilva <mateuslean123@gmail.com>
2025-12-22 11:25:42 -0300

    Remove empty code changes from Exercício 3

--- Python crie a sua primeira aplicação/Python crie a sua primeira aplicação/Exercicios/Exercício3.py
+++ Python crie a sua primeira aplicação/Python crie a sua primeira aplicação/Exercicios/Exercício3.py
@@ -42,3 +42,5 @@ num = int(input("\nDigite um número para ver sua tabela de multiplicação: "))
 for i in range(1, 11):
     resultado = num * i
     print(f"{num} x {i} = {resultado}")
+
+#Tudo Certo :)
